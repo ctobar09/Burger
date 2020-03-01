04fe4ba13ab78505c65e639930f17b288f0cd163
Caro Tobar <carotobar@Caros-MacBook-Air.local>
2020-02-29 19:32:09 -0500

    created server.js file

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Daiya');
+INSERT INTO burgers (burger_name) VALUES ('Olive');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pineapple', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Alfredo', true);
+
